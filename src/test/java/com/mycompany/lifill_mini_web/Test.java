619228797f927c3434e16f24a2937a8e80bc40ea
yu hyun ju <a01085249773@gmail.com>
2024-04-05 14:42:47 +0900

    Init : 초기 프로젝트 생성

---
+++ src/test/java/com/mycompany/lifill_mini_web/Test.java
@@ -0,0 +1,12 @@
+package com.mycompany.lifill_mini_web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
